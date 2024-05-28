3354101167fed7c1bca80b7c94bf4b0449e333b0
eliashezron <eliashezron23@gmail.com>
2024-05-28 17:59:33 +0800

    c2_blockchain p1_headers

--- src/c2_blockchain/p1_header_chain.rs
+++ src/c2_blockchain/p1_header_chain.rs
@@ -3,7 +3,7 @@
 //! start with that.
 //!
 
-use crate::hash;
+use crate::{c3_consensus::Consensus, hash};
 
 // We will use Rust's built-in hashing where the output type is u64. I'll make an alias
 // so the code is slightly more readable.
@@ -25,12 +25,24 @@ pub struct Header {
 impl Header {
     /// Returns a new valid genesis header.
     fn genesis() -> Self {
-        todo!("Exercise 1")
+        Header{
+            parent: 0,
+            height: 0,
+            extrinsics_root: (),
+            state_root: (),
+            consensus_digest: (),
+        }
     }
 
     /// Create and return a valid child header.
     fn child(&self) -> Self {
-        todo!("Exercise 2")
+        Header{
+            parent: hash(&self),
+            height: self.height + 1,
+            extrinsics_root: (),
+            state_root: (),
+            consensus_digest: (),
+        }
     }
 
     /// Verify that all the given headers form a valid chain from this header to the tip.
@@ -38,7 +50,15 @@ impl Header {
     /// This method may assume that the block on which it is called is valid, but it
     /// must verify all of the blocks in the slice;
     fn verify_sub_chain(&self, chain: &[Header]) -> bool {
-        todo!("Exercise 3")
+        // iterate through the blocks verifying if the children block heights are the same as the parent block height + 1 and the parent block hash is the same as the block has of the child block
+        let mut current = self.clone();
+        for block in chain {
+            if block.parent != hash(&current) || block.height != current.height + 1 {
+                return false;
+            }
+            current = block.clone();
+        }
+        true
     }
 }
 
@@ -46,14 +66,30 @@ impl Header {
 
 /// Build and return a valid chain with exactly five blocks including the genesis block.
 fn build_valid_chain_length_5() -> Vec<Header> {
-    todo!("Exercise 4")
+    let mut chain = Vec::new();
+    let mut g = Header::genesis();
+    chain.push(g.clone());
+    for _ in 0..4 {
+        let b = g.child();
+        chain.push(b.clone());
+        g = b;
+    }
+    chain
 }
 
 /// Build and return a chain with at least three headers.
 /// The chain should start with a proper genesis header,
 /// but the entire chain should NOT be valid.
 fn build_an_invalid_chain() -> Vec<Header> {
-    todo!("Exercise 5")
+    let mut chain = Vec::new();
+    let mut g = Header::genesis();
+    chain.push(g.clone());
+    for _ in 0..2 {
+        let b = g.child();
+        chain.push(b.clone());
+        g != b;
+    }
+    chain
 }
 
 // To run these tests: `cargo test bc_1
